10153009ca4d2e1e45e96af0b4fd14a2d50c4e5c
KiyowoGt <growidbapak@gmail.com>
2026-03-16 15:17:51 +0700

    Hardcode all API keys

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,19 +1,19 @@
 // Main Firebase config untuk Auth + Firestore (database)
 export const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyCNExvac2webq2NSDJ1JNPC9uut6TyzRXs",
+  authDomain: "studio-8281963604-c316a.firebaseapp.com",
+  projectId: "studio-8281963604-c316a",
+  storageBucket: "studio-8281963604-c316a.appspot.com",
+  messagingSenderId: "315987587283",
+  appId: "1:315987587283:web:5b3ad21d8b3de6c16b6426",
 };
 
 // Separate Firebase config untuk Storage (project berbeda)
 export const storageConfig = {
-  apiKey: process.env.NEXT_PUBLIC_STORAGE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_STORAGE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_STORAGE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_STORAGE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_STORAGE_APP_ID,
+  apiKey: "AIzaSyBskvSVKzYCxcQwYRedDp6mEwpNjGftXfc",
+  authDomain: "studio-8681629558-68f05.firebaseapp.com",
+  projectId: "studio-8681629558-68f05",
+  storageBucket: "studio-8681629558-68f05.firebasestorage.app",
+  messagingSenderId: "558334217993",
+  appId: "1:558334217993:web:559929cd726ce9bf1fcdd8",
 };
