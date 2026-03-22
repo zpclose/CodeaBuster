604c7c62398a8ad0c04ff8b25e58afce8010f556
zpclose <zpclose771@gmail.com>
2026-03-23 03:43:49 +0700

    Update sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next';
 
 export default function sitemap(): MetadataRoute.Sitemap {
-    const baseUrl = 'https://codebusters.dev';
+    const baseUrl = 'https://www.codebustersconnect.com';
     const lastModified = new Date();
 
     return [
